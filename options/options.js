9c54d3a0a36911cb2b60e44d109272f0a0256d75
Crowdbotics <team@crowdbotics.com>
2023-12-07 11:47:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "odd-mouse-44883",
+  url: "https://odd-mouse-44883.botics.co",
+  api: "https://odd-mouse-44883.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
